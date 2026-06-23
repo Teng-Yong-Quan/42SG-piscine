e5c4ac64b7f9db09663ad1f70a2d32558693db2c
root <root@TengYongQuan.localdomain>
2026-06-23 22:02:16 +0800

    Update main.c

--- C05/ex08/main.c
+++ C05/ex08/main.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yteng <yteng@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 22:00:02 by yteng             #+#    #+#             */
+/*   Updated: 2026/06/23 22:00:02 by yteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
 int	ft_ten_queens_puzzle(void);
 
-int main(void)
+int	main(void)
 {
-    printf("solution count: %d\n", ft_ten_queens_puzzle());
-    return (0);
+	printf("solution count: %d\n", ft_ten_queens_puzzle());
+	return (0);
 }
