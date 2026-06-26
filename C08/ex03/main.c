edd2f517d8c0519ed5de065c7bf43704fcb26680
root <root@TengYongQuan.localdomain>
2026-06-26 15:28:44 +0800

    Complete C08/ex03

---
+++ C08/ex03/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yteng <yteng@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/26 15:23:57 by yteng             #+#    #+#             */
+/*   Updated: 2026/06/26 15:27:48 by yteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_point.h"
+#include <stdio.h>
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int	main(void)
+{
+	t_point	point;
+
+	set_point(&point);
+	printf("x: %d\n", point.x);
+	printf("y: %d\n", point.y);
+	return (0);
+}
