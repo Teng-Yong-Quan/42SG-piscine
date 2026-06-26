cc74292d9222679dca67a71b0402f2437354917b
root <root@TengYongQuan.localdomain>
2026-06-26 16:19:23 +0800

    Remove cast on malloc

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: yteng <yteng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 11:27:15 by yteng             #+#    #+#             */
-/*   Updated: 2026/06/26 14:42:29 by yteng            ###   ########.fr       */
+/*   Updated: 2026/06/26 16:18:32 by yteng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strdup(char *src)
 	len = 0;
 	while (src[len])
 		len++;
-	dup = (char *)malloc((len + 1) * sizeof(char));
+	dup = malloc((len + 1) * sizeof(char));
 	len = 0;
 	if (!dup)
 		return (NULL);
