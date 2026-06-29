fed32a0423925bb3323c6b417009ef2d4ad9575f
root <root@TengYongQuan.localdomain>
2026-06-29 15:17:43 +0800

    Remove include <unistd.h>

--- C11/ex00/ft_foreach.c
+++ C11/ex00/ft_foreach.c
@@ -6,11 +6,9 @@
 /*   By: yteng <yteng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/29 14:52:13 by yteng             #+#    #+#             */
-/*   Updated: 2026/06/29 15:15:55 by yteng            ###   ########.fr       */
+/*   Updated: 2026/06/29 15:17:22 by yteng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
 
 void	ft_foreach(int *tab, int length, void(*f)(int))
 {
