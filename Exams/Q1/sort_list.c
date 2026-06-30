8d2072bfcd599e0e6fd82ae00722d40b58c51f0c
root <root@TengYongQuan.localdomain>
2026-06-30 14:25:54 +0800

    Complete Q1 Exam Practice

---
+++ Exams/Q1/sort_list.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_list.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yteng <yteng@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/30 11:39:22 by yteng             #+#    #+#             */
+/*   Updated: 2026/06/30 12:01:24 by yteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Write a function 'sort_list' that takes in a node pointer and
+ another function to sort the nodes respectively. 
+ Helper functions are allowed. */
+
+#include "sort_list.h"
+
+void	swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+{
+	t_list	*first;
+	t_list	*second;
+
+	first = lst;
+	while (first)
+	{
+		second = first->next;
+		while (second)
+		{
+			if (!cmp(first->data, second->data))
+				swap(&(first->data), &(second->data));
+			second = second->next;
+		}
+		first = first->next;
+	}
+	return (lst);
+}
